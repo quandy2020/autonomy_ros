4c91adf30ead76fe6d6eb931d2b8e161c1b5fd11
quandy2020 <quandy2020@126.com>
2025-08-13 16:30:06 +0800

    [Fix] Fix options
    	1 fix options
    
    Signed-off-by: duyongquan <quandy2020@126.com>

--- autonomy_ros/src/main.cpp
+++ autonomy_ros/src/main.cpp
@@ -16,15 +16,72 @@
 
 #include <memory>
 
-#include "autonomy_ros/autonomy_node.hpp"
 
+#include "autonomy/system/system.hpp"
+#include "autonomy_ros/autonomy_node.hpp"
+#include "autonomy_ros/node_options.hpp"
+
+#include "gflags/gflags.h"
+#include "tf2_ros/transform_listener.h"
+
+DEFINE_bool(collect_metrics, false,
+            "Activates the collection of runtime metrics. If activated, the "
+            "metrics can be accessed via a ROS service.");
+
+DEFINE_string(configuration_directory, "",
+              "First directory in which configuration files are searched, "
+              "second is always the Cartographer installation to allow "
+              "including files from there.");
+
+DEFINE_string(configuration_basename, "",
+              "Basename, i.e. not containing any directory prefix, of the "
+              "configuration file.");
+
+DEFINE_bool(startup_with_default_topics, true,
+  "Enable to immediately start with default topics.");
+
+namespace autonomy_ros {
+namespace {
+
+void Run() 
+{
+  rclcpp::Node::SharedPtr ros_node = rclcpp::Node::make_shared("autonomy_node");
+  constexpr double kTfBufferCacheTimeInSeconds = 10.;
+
+  auto tf_buffer = std::make_shared<tf2_ros::Buffer>(ros_node->get_clock(),
+    tf2::durationFromSec(kTfBufferCacheTimeInSeconds), ros_node);
+
+  // std::shared_ptr<tf2_ros::TransformListener> tf_listener =
+  //     std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
+
+  auto node_options = LoadOptions(FLAGS_configuration_directory, FLAGS_configuration_basename);
+  auto autonomy_builder = ::autonomy::system::CreateAutonomyBuilder(node_options.autonomy_options);
+  auto node = std::make_shared<autonomy_ros::Node>(node_options, std::move(autonomy_builder), 
+      tf_buffer, ros_node, FLAGS_collect_metrics);
+
+  if (FLAGS_startup_with_default_topics) {
+      node->StartupWithDefaultTopics();
+  }
+  rclcpp::spin(ros_node);
+}
+
+}  // namespace
+}  // namespace autonomy_ros
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  auto node = std::make_shared<autonomy_ros::Node>();
-  rclcpp::spin(node->get_node_base_interface());
-  rclcpp::shutdown();
+  google::AllowCommandLineReparsing();
+  google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, false);
 
-  return 0;
+  CHECK(!FLAGS_configuration_directory.empty())
+    << "-configuration_directory is missing.";
+  CHECK(!FLAGS_configuration_basename.empty())
+    << "-configuration_basename is missing.";
+
+  autonomy_ros::Run();
+  ::rclcpp::shutdown();
+  google::ShutdownGoogleLogging();  
+  return EXIT_SUCCESS;
 }
